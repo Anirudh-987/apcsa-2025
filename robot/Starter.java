6e9685dc4ca8026443c12b8b49913c3d10254452
Anirudh-987 <91354934+Anirudh-987@users.noreply.github.com>
2025-08-13 21:59:18 +0000

    A completed v must copy

--- robot/Starter.java
+++ robot/Starter.java
@@ -14,29 +14,55 @@ public class Starter implements Directions{
     // The line below creates a Robot that we will refer to as rob
     // Find out what the numbers and direction do!
     // Put rob in a better location for your initials.
-    Robot rob = new Robot(15,2,South,9);
-
+    Robot rob = new Robot(20,5,South,90);
+    Robot rob2 = new Robot(20, 5, South, 90);
     // Want a second robot?  No prob.  They are cheap :)
-    //Robot dude = new Robot(7,5,West,9);
+    Robot rob3 = new Robot(5,15,West,90);
+    Robot rob4 = new Robot(5,15, West, 90);
+    Robot rob5 = new Robot(10,7, East, 90);
     // examples of commands you can invoke on a Robot
-    rob.move();// move one step in the direction it is facing
+    // move one step in the direction it is facing
+
+    for (int i=0; i < 6; i++)  {
+    rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
+    rob.move();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.move();
+    rob.putBeeper();
+    rob.turnLeft();
+
+    rob2.move();
+    rob2.putBeeper();
+    rob2.move();
+    rob2.move();
+    rob2.turnLeft();
+    rob2.move();
+    rob2.putBeeper();
+    rob2.turnLeft();
+    rob2.turnLeft();
+    rob2.turnLeft();
+
+    }
+
+    rob5.move();
+    rob5.putBeeper();
+    rob5.move();
+    rob5.putBeeper();
+    rob5.move();
+    rob5.putBeeper();
+    rob5.move();
+  
+    for (int j=0, j < 6, i++)  {
+      
 
 
-    // starting the letter R
-    rob.putBeeper();
-    rob.putBeeper();
-    rob.putBeeper();
-    rob.putBeeper();
-    rob.move();
-    rob.putBeeper();
-    rob.move();
-    rob.move();
-    rob.move();
-    rob.putBeeper();
-    rob.move();
-    rob.move();
-    rob.putBeeper();
 
+    }
     // done with the line, now on the curve
     // rob.turnLeft();
 
